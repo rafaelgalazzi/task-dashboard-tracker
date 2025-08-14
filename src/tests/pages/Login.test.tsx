f9c625347f3b918f904e3ddf2e5822090d4ff3bb
RafaelGalazzi <rafaelgalazzi@ucl.br>
2025-08-14 01:23:12 -0300

    fixed test

--- src/tests/pages/Login.test.tsx
+++ src/tests/pages/Login.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { vi } from 'vitest';
-import LoginScreen from '../../pages/Login/LoginScreen';
+import { LoginScreen } from '../../pages/Login/LoginScreen';
 
 // Mock do React Router
 vi.mock('react-router-dom', () => ({
