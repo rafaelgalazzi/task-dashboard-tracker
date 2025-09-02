4bc3456861bacde9f2e1e52fbf3c0ece9dd82234
RafaelGalazzi <rafaelgalazzi@ucl.br>
2025-09-02 18:21:17 -0300

    Fix: loading spinner component

--- src/components/Spinner/LoadingSpinner.tsx
+++ src/components/Spinner/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 export function LoadingSpinner() {
   return (
     <div className="flex items-center justify-center w-full h-full">
-      <span className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
+      <span className="w-[20px] h-[20px] border-4 border-blue-500 border-t-transparent rounded-full animate-spin" />
     </div>
   );
 }
