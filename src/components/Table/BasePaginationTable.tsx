098ccaa3c05310046347fae1de2c2e773cf3fb83
RafaelGalazzi <rafaelgalazzi@ucl.br>
2025-09-07 14:04:37 -0300

    fixed import

--- src/components/Table/BasePaginationTable.tsx
+++ src/components/Table/BasePaginationTable.tsx
@@ -1,4 +1,4 @@
-import React, { type ReactElement } from 'react';
+import { type ReactElement } from 'react';
 import BaseText from '../Text/BaseText';
 import { useScreen } from '../../hooks/useScreen';
 
