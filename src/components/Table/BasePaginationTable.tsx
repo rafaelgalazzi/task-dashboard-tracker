e1b2dfeacf9d1a6a2807cb3d8ba588e50e70df8d
RafaelGalazzi <rafaelgalazzi@ucl.br>
2025-09-07 13:57:30 -0300

    added base pagination

---
+++ src/components/Table/BasePaginationTable.tsx
@@ -0,0 +1,79 @@
+import React, { type ReactElement } from 'react';
+import BaseText from '../Text/BaseText';
+import { useScreen } from '../../hooks/useScreen';
+
+interface BasePaginationTableProps<T> {
+  columns: (keyof T)[];
+  items: T[];
+  page: number; // página atual
+  pageSize: number; // itens por página
+  total: number; // total de itens
+  onPageChange: (page: number) => void; // callback para mudar página
+}
+
+export function BasePaginationTable<T>({
+  columns,
+  items,
+  page,
+  pageSize,
+  total,
+  onPageChange,
+}: BasePaginationTableProps<T>) {
+  const { isMobile } = useScreen();
+
+  if (!items.length) return <BaseText>No items</BaseText>;
+
+  const totalPages = Math.ceil(total / pageSize);
+
+  let tableHtml: ReactElement = <></>;
+
+  if (isMobile) {
+    tableHtml = (
+      <div className="border rounded-md">
+        <div className="flex bg-accent font-semibold text-center">
+          {columns.map((column, idx) => (
+            <div key={idx} className="flex-1 p-2 border-r last:border-r-0">
+              {String(column)}
+            </div>
+          ))}
+        </div>
+
+        {items.map((item, rowIdx) => (
+          <div key={`row-${rowIdx}`} className="flex text-center border-b last:border-b-0">
+            {columns.map((column, colIdx) => (
+              <div key={colIdx} className="flex-1 p-2 border-r last:border-r-0">
+                {String(item[column])}
+              </div>
+            ))}
+          </div>
+        ))}
+
+        <div className="flex justify-between items-center ">
+          <button
+            className="px-3 py-1 border rounded disabled:opacity-50"
+            disabled={page === 1}
+            onClick={() => onPageChange(page - 1)}
+          >
+            Anterior
+          </button>
+
+          <span>
+            Página {page} de {totalPages}
+          </span>
+
+          <button
+            className="px-3 py-1 border rounded disabled:opacity-50"
+            disabled={page === totalPages}
+            onClick={() => onPageChange(page + 1)}
+          >
+            Próximo
+          </button>
+        </div>
+      </div>
+    );
+  } else {
+    tableHtml = <></>
+  }
+
+  return tableHtml;
+}
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,9 +1,49 @@
+import BaseCard from '../../components/Layout/BaseCard';
+import { BasePaginationTable } from '../../components/Table/BasePaginationTable';
 import BaseText from '../../components/Text/BaseText';
 
+interface User {
+  name: string;
+  description: string;
+}
+
 export function HomePage() {
+  function changePageHandler() {
+    return;
+  }
+
+  const items: User[] = [
+    {
+      name: 'batata',
+      description: 'teste',
+    },
+    {
+      name: 'batata',
+      description: 'teste',
+    },
+    {
+      name: 'batata',
+      description: 'teste',
+    },
+  ];
+
+  const columns: (keyof User)[] = ['name', 'description'];
+
   return (
     <div>
-      <BaseText className="text-2xl font-bold">Home Dashboard</BaseText>
+      <BaseText className="text-2xl font-bold">Projects</BaseText>
+      <div className="flex">
+        <BaseCard className="md:w-1/2 w-full">
+          <BasePaginationTable
+            columns={columns}
+            items={items}
+            page={1}
+            pageSize={10}
+            total={0}
+            onPageChange={changePageHandler}
+          />
+        </BaseCard>
+      </div>
     </div>
   );
 }
